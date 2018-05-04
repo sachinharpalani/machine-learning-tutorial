fdbd624d6e0ed1026448f2ed7c42b04d3c2d8e51
Sachin Harpalani <sachinharpalani3795@gmail.com>
2018-05-04 15:23:32 +0530

    Added code on r_squared and coefficient of determination

--- linear_regression_algorithm.py
+++ linear_regression_algorithm.py
@@ -23,12 +23,29 @@ def best_fit_slope_and_intercept(xs,ys):
     b = mean(ys) - m*mean(xs)
     return m,b
 
+def squared_error(ys_orig,ys_line):
+    return sum((ys_line-ys_orig)**2)
+
+#Formula for coefficient of determination
+# r^2 = 1 - SE(y^)
+#           ------
+#           SE of mean(Y)
+#Used to check how good is the best fit line
+def coefficient_of_determination(ys_orig,ys_line):
+    ys_mean_line=[mean(ys_orig) for y in ys_orig]
+    squared_error_regr = squared_error(ys_orig,ys_line)
+    squared_error_y_mean = squared_error(ys_orig,ys_mean_line)
+    return 1 - (squared_error_regr / squared_error_y_mean)
+
 m,b = best_fit_slope_and_intercept(xs,ys)
 
 regression_line = [(m*x)+b for x in xs]
 
 predict_x = 8
 predict_y = (m*predict_x) + b
+
+r_squared = coefficient_of_determination(ys,regression_line)
+print(r_squared)
 
 plt.scatter(xs,ys)
 plt.scatter(predict_x, predict_y, color='g')
