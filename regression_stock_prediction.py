2971cfb365f6955205c4a29368ae5ff0917f155f
Sachin Harpalani <sachinharpalani3795@gmail.com>
2018-05-04 12:02:04 +0530

    Added code on prediction and visualize forecast in graph using matplotlib

--- regression_stock_prediction.py
+++ regression_stock_prediction.py
@@ -1,10 +1,14 @@
 import os
 import pandas as pd
-import quandl, math
+import quandl, math, datetime
 quandl.ApiConfig.api_key = os.environ.get('QUANDL_API_KEY')
 import numpy as np
 from sklearn import preprocessing, model_selection, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 df = quandl.get('WIKI/GOOGL')
 df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
@@ -20,20 +24,39 @@ forecast_out = int(math.ceil(0.01*len(df)))
 print(forecast_out)
 
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
 
 X = np.array(df.drop(['label'],1))
-y = np.array(df['label'])
-
 X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
+
+
 df.dropna(inplace=True)
 y = np.array(df['label'])
-
+y = np.array(df['label'])
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X,y,test_size=0.2)
 
 clf = LinearRegression()
 #clf = svm.SVR()
 clf.fit(X_train,y_train)
 accuracy = clf.score(X_test,y_test)
+forecast_set = clf.predict(X_lately)
+print(forecast_set, accuracy, forecast_out)
+df['Forecast'] = np.nan
 
-print(accuracy)
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df['adj. close'].plot()
+df['forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('date')
+plt.ylabel('price')
+plt.show()
