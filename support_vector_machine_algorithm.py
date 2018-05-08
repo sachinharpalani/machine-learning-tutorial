42147b2ea67bfe00f91d6afa1771358446cb3197
Sachin Harpalani <sachinharpalani3795@gmail.com>
2018-05-08 15:28:49 +0530

    Added code on predict and visualize and tested with sample dataset

--- support_vector_machine_algorithm.py
+++ support_vector_machine_algorithm.py
@@ -35,7 +35,8 @@ class Support_Vector_Machine:
         step_sizes = [self.max_feature_value * 0.1,
                       self.max_feature_value * 0.01,
                       #point of expense:
-                      self.max_feature_value * 0.001,]
+                      self.max_feature_value * 0.001,
+                      ]
 
         #extremely expensive
         b_range_multiple = 5
@@ -49,7 +50,7 @@ class Support_Vector_Machine:
             # we can do this because convex
             optimized = False
             while not optimized:
-                for b in np.arrange(-1*(self.max_feature_value * b_range_multiple),\
+                for b in np.arange(-1*(self.max_feature_value * b_range_multiple),\
                                     self.max_feature_value * b_range_multiple, \
                                     step*b_multiple):
                     for transformations in transforms:
@@ -58,8 +59,7 @@ class Support_Vector_Machine:
                         #weakest link in SVM fundamentally
                         #SMO attempts to fix this
                         # yi(xi.w+b) >= 1
-                        #
-                        ### add break to optimize below for loop
+
                         for yi in self.data:
                             for xi in self.data[yi]:
                                 if not yi*(np.dot(w_t,xi)+b) >= 1:
@@ -82,12 +82,53 @@ class Support_Vector_Machine:
             self.b = opt_choice[1]
             latest_optimum = opt_choice[0][0] + step*2
 
+        #To see optimization steps:
+        for yi in self.data:
+            for xi in self.data[yi]:
+                print(xi, ':', yi*(np.dot(self.w,xi)+self.b))
+
 
     def predict(self,features):
         # sign(x.w+b)
         classification = np.sign(np.dot(np.array(features),self.w)+self.b)
-
+        if classification != 0 and self.visualization:
+            self.ax.scatter(features[0], features[1], s=200, marker='*', c=self.colors[classification])
         return classification
+
+    def visualize(self):
+        [[self.ax.scatter(x[0],x[1],s=100,color=self.colors[i]) for x in data_dict[i]] for i in data_dict]
+
+        #hyperplane = x.w+b
+        # v = x.w+b
+        #psv = 1
+        #nsv = -1
+        #dec = 0
+        def hyperplane(x,w,b,v):
+            return (-w[0]*x-b+v) / w[1]
+
+        datarange = (self.min_feature_value*0.9,self.max_feature_value*1.1)
+        hyp_x_min = datarange[0]
+        hyp_x_max = datarange[1]
+
+        # (w.x+b) = 1
+        #positive support vector hyperplane
+        psv1 = hyperplane(hyp_x_min,self.w,self.b,1)
+        psv2 = hyperplane(hyp_x_max,self.w,self.b,1)
+        self.ax.plot([hyp_x_min,hyp_x_max],[psv1,psv2], 'k')
+
+        # (w.x+b) = -1
+        #negative support vector hyperplane
+        nsv1 = hyperplane(hyp_x_min,self.w,self.b,-1)
+        nsv2 = hyperplane(hyp_x_max,self.w,self.b,-1)
+        self.ax.plot([hyp_x_min,hyp_x_max],[nsv1,nsv2], 'k')
+
+        # (w.x+b) = 0
+        # decision boundary
+        db1 = hyperplane(hyp_x_min,self.w,self.b,0)
+        db2 = hyperplane(hyp_x_max,self.w,self.b,0)
+        self.ax.plot([hyp_x_min,hyp_x_max],[db1,db2], 'y--')
+
+        plt.show()
 
 data_dict = {-1:np.array([[1,7],
                           [2,8],
@@ -96,7 +137,20 @@ data_dict = {-1:np.array([[1,7],
                          [6,-1],
                          [7,3],])}
 
-#To visualize the dataset points
-# [plt.scatter(i[0],i[1],color='g') for i in data_dict[-1]]
-# [plt.scatter(i[0],i[1],color='r') for i in data_dict[1]]
-# plt.show()
+
+svm = Support_Vector_Machine()
+svm.fit(data=data_dict)
+
+predict_us = [[0,10],
+              [1,3],
+              [3,4],
+              [3,5],
+              [5,5],
+              [5,6],
+              [6,-5],
+              [5,8]]
+
+for p in predict_us:
+    svm.predict(p)
+
+svm.visualize()
