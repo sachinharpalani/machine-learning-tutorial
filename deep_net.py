b8eff49d54e02a2d45fa4b90b5b23cee896464f0
Sachin Harpalani <sachinharpalani3795@gmail.com>
2018-05-09 16:39:31 +0530

    Added code to train neural network and calculate accuracy

--- deep_net.py
+++ deep_net.py
@@ -13,7 +13,14 @@ feed forward + backpropogation = epoch
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
+#This is used to ignore warnings of depricated methods of 'read_data_sets'
+old_v = tf.logging.get_verbosity()
+tf.logging.set_verbosity(tf.logging.ERROR)
+
 mnist = input_data.read_data_sets('/tmp/data/', one_hot=True)
+
+#This restores the verbosity value
+tf.logging.set_verbosity(old_v)
 
 n_nodes_hl1 = 500
 n_nodes_hl2 = 500
@@ -31,28 +38,54 @@ def neural_network_model(data):
     #biases are used so that even if input_data is 0, some neurons are fired since it adds value
 
     hidden_1_layer = {'weights' : tf.Variable(tf.random_normal([784, n_nodes_hl1])),
-                      'biases': tf.Variable(tf.random_normal(n_nodes_hl1))}
+                      'biases': tf.Variable(tf.random_normal([n_nodes_hl1]))}
 
     hidden_2_layer = {'weights' : tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
-                      'biases': tf.Variable(tf.random_normal(n_nodes_hl2))}
+                      'biases': tf.Variable(tf.random_normal([n_nodes_hl2]))}
 
     hidden_3_layer = {'weights' : tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
-                      'biases': tf.Variable(tf.random_normal(n_nodes_hl3))}
+                      'biases': tf.Variable(tf.random_normal([n_nodes_hl3]))}
 
     output_layer = {'weights' : tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
                       'biases': tf.Variable(tf.random_normal([n_classes]))}
 
     #(input_data * weights) + biases
 
-    l1 = tf.add(tf.matmul(data, hidden_1_layer['weights']) + hidden_1_layer['biases'])
+    l1 = tf.add(tf.matmul(data, hidden_1_layer['weights']), hidden_1_layer['biases'])
     l1 = tf.nn.relu(l1)
 
-    l2 = tf.add(tf.matmul(l1, hidden_2_layer['weights']) + hidden_2_layer['biases'])
+    l2 = tf.add(tf.matmul(l1, hidden_2_layer['weights']), hidden_2_layer['biases'])
     l2 = tf.nn.relu(l2)
 
-    l3 = tf.add(tf.matmul(l2, hidden_3_layer['weights']) + hidden_3_layer['biases'])
+    l3 = tf.add(tf.matmul(l2, hidden_3_layer['weights']), hidden_3_layer['biases'])
     l3 = tf.nn.relu(l3)
 
-    output = tf.matmul(data, output_layer['weights']) + output_layer['biases']
+    output = tf.add(tf.matmul(l3, output_layer['weights']), output_layer['biases'])
 
     return output
+
+def train_neural_network(x):
+    prediction = neural_network_model(x)
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits = prediction, labels = y))
+    optimizer = tf.train.AdamOptimizer().minimize(cost)
+
+    # cycles feed forward + backpropogation
+    hm_epochs = 10
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+
+        for epoch in range(hm_epochs):
+            epoch_loss = 0
+            for _ in range(int(mnist.train.num_examples/batch_size)):
+                epoch_x,epoch_y = mnist.train.next_batch(batch_size)
+                _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
+                epoch_loss += c
+            print('Epoch',epoch, 'completed out of ', hm_epochs, 'loss:',epoch_loss)
+
+        correct = tf.equal(tf.argmax(prediction,1), tf.argmax(y,1))
+
+        accuracy = tf.reduce_mean(tf.cast(correct,'float'))
+        print('Accuracy:',accuracy.eval({x:mnist.test.images,y:mnist.test.labels}))
+
+train_neural_network(x)
